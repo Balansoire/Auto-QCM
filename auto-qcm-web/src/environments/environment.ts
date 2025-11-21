57372e8ac5ec45ee461443365363b2e81c12b023
Théotime DUSERRE <98874870+Balansoire@users.noreply.github.com>
2025-11-21 15:57:44 +0100

    remove  angular env from gitignore

---
+++ auto-qcm-web/src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  apiBaseUrl: 'http://localhost:8000',
+  supabaseUrl: 'https://hljodbgwmkupqijqnozd.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhsam9kYmd3bWt1cHFpanFub3pkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMzODk4NjMsImV4cCI6MjA3ODk2NTg2M30.d0-uPJGaCKyWOxXklLQqm1lsPfDsbJDObNqbWJ5lul4',
+  netlifySite: 'https://auto-qcm.netlify.app',
+};
