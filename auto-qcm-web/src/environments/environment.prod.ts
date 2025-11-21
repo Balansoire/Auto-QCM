1a32571ad96b5c0dc6750a3ba456bb2389809e33
Théotime DUSERRE <98874870+Balansoire@users.noreply.github.com>
2025-11-21 16:57:28 +0100

    use correct backend link

--- auto-qcm-web/src/environments/environment.prod.ts
+++ auto-qcm-web/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://auto-qcm-api.onrender.com',
+  apiBaseUrl: 'https://auto-qcm.onrender.com',
   supabaseUrl: 'https://hljodbgwmkupqijqnozd.supabase.co',
   supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhsam9kYmd3bWt1cHFpanFub3pkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMzODk4NjMsImV4cCI6MjA3ODk2NTg2M30.d0-uPJGaCKyWOxXklLQqm1lsPfDsbJDObNqbWJ5lul4',
   netlifySite: 'https://auto-qcm.netlify.app',
