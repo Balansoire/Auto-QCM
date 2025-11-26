486e93383a93a45ff0ace594bcf3cf99d7153c81
Théotime DUSERRE <98874870+Balansoire@users.noreply.github.com>
2025-11-26 15:34:28 +0100

    add error handling to auth guard

--- auto-qcm-web/src/app/shared/auth.guard.ts
+++ auto-qcm-web/src/app/shared/auth.guard.ts
@@ -3,7 +3,16 @@ import { CanActivateFn, Router } from '@angular/router';
 import { supabase } from './supabase-client';
 
 export const authGuard: CanActivateFn = async () => {
-  const { data } = await supabase.auth.getSession();
-  if (data.session) return true;
-  return inject(Router).createUrlTree(['/login']);
+  try {
+    const { data, error } = await supabase.auth.getSession();
+    if (error) {
+      console.error(error);
+      return inject(Router).createUrlTree(['/login']);
+    }
+    if (data.session) return true;
+    return inject(Router).createUrlTree(['/login']);
+  } catch (e) {
+    console.error(e);
+    return inject(Router).createUrlTree(['/login']);
+  }
 };
